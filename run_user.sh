e972a5b66cc53816e9d936f74f243429fea09235
tomamints <shirai@mints.works>
2026-03-31 01:00:05 +0900

    OS development: Fix ecall and clean up logs

--- run_user.sh
+++ run_user.sh
@@ -17,5 +17,8 @@ $OBJCOPY -I binary -O elf32-littleriscv -B riscv:rv32 shell.bin shell.bin.o
 $CC $CFLAGS -Wl,-T,kernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c common.c shell.bin.o
 
 # 実行
-qemu-system-riscv32 -machine virt -bios default -nographic -serial mon:stdio \
+qemu-system-riscv32 -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+  -d unimp,guest_errors,int,cpu_reset -D qemu.log \
+  -drive id=drive0,file=lorem.txt,format=raw,if=none \
+  -device virtio-blk-device,drive=drive0,bus=virtio-mmio-bus.0 \
   -kernel kernel.elf
--- user.h
+++ user.h
@@ -2,4 +2,7 @@
 #include "common.h"
 
 __attribute__((noreturn)) void exit(void);
+
 void putchar(char ch);
+
+int getchar(void);
